cc9b0223f1b20f2c1e1c201b6dccaaa20cebe8a6
LouisTao <taofengyejx@gmail.com>
2023-07-11 15:22:02 -0500

    interface app added

---
+++ interface-chatBot/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+
+import sys
+from pathlib import Path
+# Add the parent directory to the Python module search path
+sys.path.append(str(Path(__file__).resolve().parent.parent / 'LLM_Dev'))
+from few_shot import get_response
+
+
+app = Flask(__name__)
+
+
+
+@app.get('/') 
+def index_get():
+    return render_template('index.html')
+
+
+@app.post('/chat')
+def predict():
+    text = request.get_json().get('message')
+    
+    #get response from model
+    response = "get_response(text)"
+    message = {'answer': response}
+    
+    return jsonify(message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
