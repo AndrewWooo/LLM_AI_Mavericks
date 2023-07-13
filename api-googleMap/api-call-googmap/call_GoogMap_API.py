d675462541855205ada406c6aa78eba2256cb44e
gerryhzgEzoic <ghuang@ezoic.com>
2023-07-13 17:16:10 -0500

    add the api-call-googmap to k8s

--- api-googleMap/api-call-googmap/call_GoogMap_API.py
+++ api-googleMap/api-call-googmap/call_GoogMap_API.py
@@ -1,7 +1,10 @@
 import requests
+from pprint import pprint
+import time
 
 # Set the API endpoint URL
-url = 'http://169.254.97.204:5000/search'  
+# url = 'http://169.254.97.204:5000/search'
+url = 'http://googmap-service:5000/search'
 
 # Set the parameters: doctorType and address
 doctorTpye = "orthodontists"
@@ -19,8 +22,16 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     # Successful response
     data = response.json()
+    # Pretty-print the data
+    pprint(data)
     # Process the data returned by the API
     # ...
 else:
     # Error response
     print(f"API error: {response.status_code} - {response.json()['error']}")
+
+# Sleep for one hour
+time.sleep(60*60)
+
+# Exit
+exit(0)
