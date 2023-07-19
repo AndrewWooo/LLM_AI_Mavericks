46a73f7b61798d7fdcd3c1c22558d1284ff700d0
LouisTao <taofengyejx@gmail.com>
2023-07-19 17:15:38 -0500

    reasons for category

--- chatbot-backend/in_context_learning-msg-api.py
+++ chatbot-backend/in_context_learning-msg-api.py
@@ -106,10 +106,10 @@ assistant.messages.append({"role": "system", "content": f"Patient Age: {patient_
 assistant.messages.append({"role": "system", "content": f"Patient Gender: {patient_info['gender']}"})
 assistant.messages.append({"role": "system", "content": f"Patient Medical Records: {patient_info['medical_records']}"})
 
-# print("Assistant: Hello, what brought you here today?")
-jsonify({"assistant_reply": "Hello, what brought you here today?"})
-# patient_input = input("Patient: ")
-patient_input = request.json['patient_input']
+print("Assistant: Hello, what brought you here today?")
+#jsonify({"assistant_reply": "Hello, what brought you here today?"})
+patient_input = input("Patient: ")
+#patient_input = request.json['patient_input']
 # Add the patient's input to the conversation
 assistant.messages.append({
     "role": "user",
@@ -137,8 +137,8 @@ while True:
     assistant_reply = response.choices[0].message['content']
 
     # Print the assistant's reply
-    # print("Assistant:", assistant_reply)
-    jsonify({"assistant_reply": assistant_reply})
+    print("Assistant:", assistant_reply)
+    #jsonify({"assistant_reply": assistant_reply})
 
     # Extract the predicted category from the assistant's final reply
     predicted_category = assistant.predict_category(assistant_reply)
@@ -147,8 +147,8 @@ while True:
         break
 
     # Get additional patient input
-    # patient_input = input("Patient: ")
-    patient_input = request.json['patient_input']
+    patient_input = input("Patient: ")
+    #patient_input = request.json['patient_input']
 
     # Add assistant and patient responses to the conversation
     assistant.messages.append({
@@ -162,45 +162,47 @@ while True:
     })
 
     # Increment the turn counter
-    turns += 1
+    turns += 1 
+
+
 
 # Print the predicted category
 print("Predicted category:", predicted_category)
 
-# print(f"Do you want to make an appointment with a {predicted_category} Medical Doctor? (y/n)")
-jsonify({"assistant_reply": f"Do you want to make an appointment with a {predicted_category} Medical Doctor? (y/n)"})
+print(f"Do you want to make an appointment with a {predicted_category} Medical Doctor? (y/n)")
+#jsonify({"assistant_reply": f"Do you want to make an appointment with a {predicted_category} Medical Doctor? (y/n)"})
 
 
 assistant_reply
-# appointment_or_not = input("Patient: ")
-patient_input = request.json['patient_input']
+appointment_or_not = input("Patient: ")
+#patient_input = request.json['patient_input']
 
 if appointment_or_not == 'n':
-  # print("goodbye")
-  jsonify({"assistant_reply": "Since you do not need appointment. It is time to say Goodbye"})
+  print("goodbye")
+  #jsonify({"assistant_reply": "Since you do not need appointment. It is time to say Goodbye"})
 else:
   doctor_recom = search_doctors(predicted_category, patient_address)
   dr_name = doctor_recom[0]
   err = 0
   while dr_name == 'error':
     if err > 2:
-      # print(f"Assistant:It seems the system has some issue, please drop this chat to find other way to solve your problem")
-      jsonify({"assistant_reply": f"Assistant:It seems the system has some issue, please drop this chat to find other way to solve your problem"})
-    # print(f"Assistant: It seems that your address is not right, please input your address again.")
-    jsonify({"assistant_reply": "It seems that your address is not right, please input your address again."})
-    # new_address = input("Patient: ")
-    patient_input = request.json['patient_input']
+      print(f"Assistant:It seems the system has some issue, please drop this chat to find other way to solve your problem")
+      # jsonify({"assistant_reply": f"Assistant:It seems the system has some issue, please drop this chat to find other way to solve your problem"})
+      print(f"Assistant: It seems that your address is not right, please input your address again.")
+    #jsonify({"assistant_reply": "It seems that your address is not right, please input your address again."})
+    new_address = input("Patient: ")
+    #patient_input = request.json['patient_input']
     doctor_recom = search_doctors(predicted_category, patient_address)
     err += 1
   dr_address = doctor_recom[1]
-  # print(f"""The clinic/doctor name is {dr_name}, \n
-  #   whose address is \n 
-  #   {dr_address}""")
-  jsonify({"assistant_reply": f"""The clinic/doctor name is {dr_name}, \n
-    whose address is \n 
-    {dr_address}"""})
-  # print(f"Assistant: You are going to receive a confirmation email about your appointment")
-  jsonify({"assistant_reply": "You are going to receive a confirmation email about your appointment"})
+  print(f"""The clinic/doctor name is {dr_name}, \n
+     whose address is \n 
+     {dr_address}""")
+  #jsonify({"assistant_reply": f"""The clinic/doctor name is {dr_name}, \n
+  #  whose address is \n 
+  #  {dr_address}"""})
+  print(f"Assistant: You are going to receive a confirmation email about your appointment")
+  #jsonify({"assistant_reply": "You are going to receive a confirmation email about your appointment"})
   subject = "Doctor Appoint confirmation"
   message = f"""{assistant_reply} \n
               The clinic/doctor's name is {dr_name}, whose address is {dr_address} \n
@@ -208,8 +210,8 @@ else:
               """
   send_email(sender_email, sender_password, receiver_email, subject, message)
 
-jsonify({"assistant_reply": "You are all set. Goodbye!"
-# print("Assistant: Goodbye!")
+#jsonify({"assistant_reply": "You are all set. Goodbye!"})
+print("Assistant: Goodbye!")
 
 
 
