2dbc63775fd7e9365c27a649122bc30f6cb16dc8
VectorLambda <yujieli2016@gmail.com>
2023-08-20 21:01:59 -0400

    I have the doctor info outside of my folder.

--- webscrapping-doctorsProfile/jsonConvert.py
+++ webscrapping-doctorsProfile/jsonConvert.py
@@ -7,7 +7,7 @@ import json
  
 # the file to be converted to
 # json format
-filename = 'doctor.txt'
+filename = 'webscrapping-doctorsProfile/doctor.txt'
  
 # dictionary where the lines from
 # text will be stored
@@ -59,6 +59,6 @@ with open(filename) as fh:
  
 # creating json file
 # the JSON file is named as test1
-out_file = open("test1.json", "w")
+out_file = open("doctor_info.json", "w")
 json.dump(dict1, out_file, indent = 4, sort_keys = False)
 out_file.close()
